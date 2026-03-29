5560f0b9242eea0e2d1e5216bdb35f43db9936e5
Dalai-02 <dalaipacheco3gmail.com>
2026-03-28 22:47:28 -0600

    fix: added user_id to patient fillable to allow job to create models

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Patient extends Model
 {
     protected $fillable = [
+        'user_id',
         'date_of_birth',
         'blood_type_id',
         'allergies',
